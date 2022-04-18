8612c0b9d793dd471f76fb43f2d15a3df68682ba
Evie Dickman <GeneralEvie9@outlook.com>
2022-04-18 18:04:33 -0500

    arrow flip

--- main.js
+++ main.js
@@ -4,5 +4,6 @@ $(document).ready(() =>
     $('h2').click(function()
     {
         $(event.currentTarget).next('.As').slideToggle();
+        $(event.currentTarget).children('.arrows').toggleClass('flip-arrow')
     });
 });
