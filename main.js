aeacf30b1b2db13028f4a8abc5b75e90735a220b
Evie Dickman <GeneralEvie9@outlook.com>
2022-04-18 18:23:33 -0500

    current active

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ $(document).ready(() =>
     
     $('h2').click(function()
     {
+        $(event.currentTarget).toggleClass('bold')
         $(event.currentTarget).next('.As').slideToggle();
         $(event.currentTarget).children('.arrows').toggleClass('flip-arrow')
     });
