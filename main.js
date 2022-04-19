eac043850620b453fb738c5bb906918ac4d875dc
Evie Dickman <GeneralEvie9@outlook.com>
2022-04-18 20:01:01 -0500

    finished desktop

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ $(document).ready(() =>
     $('h2').click(function()
     {
         $(event.currentTarget).toggleClass('bold')
+        $(event.currentTarget).toggleClass('remove-border')
         $(event.currentTarget).next('.As').slideToggle();
         $(event.currentTarget).children('.arrows').toggleClass('flip-arrow')
+
     });
 });
